703fbe46157c6188b35b900d336b83d5f31a582b
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-03-09 16:47:57 +0100

    add resolução de Nivel01/Exercicio01

--- Nivel01/Exercicio01/Program.cs
+++ Nivel01/Exercicio01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Exercicio01 Hello World!");
+﻿/*
+Faça um programa para calcular o estoque médio de uma peça, sendo que:
+ESTOQUE MÉDIO = (QUANTIDADE_MÍNIMA + QUANTIDADE_MÁXIMA) / 2.
+*/
+// variaveis
+int QUANTIDADE_MINIMA;
+int QUANTIDADE_MAXIMA;
+int ESTOQUE_MEDIO;
+
+// programa
+
+// exibe a mensagem
+Console.WriteLine("Quantidade mínima: ");
+// recebe o valor do teclado (String), converte para inteiro e o armazena na variável QUANTIDADE_MINIMA
+QUANTIDADE_MINIMA = int.Parse(Console.ReadLine());
+
+// exibe a mensagem
+Console.WriteLine("Quantidade máxima: ");
+// recebe o valor do teclado
+QUANTIDADE_MAXIMA = int.Parse(Console.ReadLine());
+
+// calcula o estoque médio e o armazena na variável ESTOQUE_MEDIO
+ESTOQUE_MEDIO = (QUANTIDADE_MINIMA + QUANTIDADE_MAXIMA) / 2;
+Console.WriteLine($"Estoque médio: " + ESTOQUE_MEDIO);
+
+// fim do programa
